667d318386914745ab89a817adabe21777ae2dc3
Raphael do Vale A. Gomes <raphael@dovale.net>
2018-12-11 23:29:13 -0200

    Few changes due to review

--- src/main/java/net/dovale/Application.java
+++ src/main/java/net/dovale/Application.java
@@ -19,65 +19,34 @@ public class Application {
         StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
                 .configure()
                 .build();
-
         try (SessionFactory sessionFactory = new MetadataSources(registry)
                 .buildMetadata()
                 .buildSessionFactory()
         ) {
-
             CourseDAO courseDAO = new CourseDAO(sessionFactory);
             TeacherDAO teacherDAO = new TeacherDAO(sessionFactory);
-
             try (Session session = sessionFactory.getCurrentSession()) {
-
                 Transaction tx = session.beginTransaction();
-
                 // create teachers
-                Teacher pj = new Teacher(
-                        "Profesor Jirafales",
-                        "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d1/Ruben2017.jpg/245px-Ruben2017.jpg",
-                        "jirafales@yahoo_.com"
-                );
-
-                Teacher px = new Teacher(
-                        "Professor X",
-                        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9uI1Cb-nQ2uJOph4_t96KRvLSMjczAKnHLJYi1nqWXagvqWc4",
-                        "director@xproject_.com"
-
-                );
-
+                Teacher pj = new Teacher("Profesor Jirafales","https://upload.wikimedia.org/wikipedia/commons/thumb/d/d1/Ruben2017.jpg/245px-Ruben2017.jpg","jirafales@example.com");
+                Teacher px = new Teacher("Professor X","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9uI1Cb-nQ2uJOph4_t96KRvLSMjczAKnHLJYi1nqWXagvqWc4","director@xproject_.com");
                 teacherDAO.save(pj);
                 teacherDAO.save(px);
-
-                // create courses
-                Course mathematics = new Course("Mathematics", 20, (short) 10, pj);
-                Course spanish = new Course("Spanish", 20, (short) 10, pj);
-                Course dealingWithUnknown = new Course("Dealing with unknown", 10, (short) 100, pj);
-                Course handlingYourMentalPower = new Course("Handling your mental power", 50, (short) 100, pj);
-                Course introductionToPsychology = new Course("Introduction to psychology", 90, (short) 100, pj);
-
-                courseDAO.save(mathematics);
-                courseDAO.save(spanish);
-                courseDAO.save(dealingWithUnknown);
-                courseDAO.save(handlingYourMentalPower);
-                courseDAO.save(introductionToPsychology);
-
+                courseDAO.save(new Course("Mathematics", 20, (short) 10, pj));
+                courseDAO.save(new Course("Spanish", 20, (short) 10, pj));
+                courseDAO.save(new Course("Dealing with unknown", 10, (short) 100, pj));
+                courseDAO.save(new Course("Handling your mental power", 50, (short) 100, pj));
+                courseDAO.save(new Course("Introduction to psychology", 90, (short) 100, pj));
                 tx.commit();
             }
-
             try (Session session = sessionFactory.getCurrentSession()) {
                 session.beginTransaction();
                 System.out.println("Courses");
-                courseDAO
-                        .list()
-                        .forEach(course -> System.out.println(course.getName()));
+                courseDAO.list().forEach(course -> System.out.println(course.getName()));
 
                 System.out.println("Teachers");
-                teacherDAO
-                        .list()
-                        .forEach(teacher -> System.out.println(teacher.getName()));
+                teacherDAO.list().forEach(teacher -> System.out.println(teacher.getName()));
             }
-
         }
 
     }
